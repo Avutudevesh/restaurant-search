cdc3489f1e2bc8de3a5781657973ce9ce944fe48
Devesh Avutu <devesh14may@gmail.com>
2020-04-18 17:03:17 +0530

    created results list component

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -2,11 +2,17 @@ import React, { useState, useEffect } from "react";
 import { Text, View, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
 import useResults from "../hooks/useResults";
+import ResultsList from "../components/ResultsList";
 
 const SearchScreen = () => {
 	const [term, setTerm] = useState("");
 	const [searchApi, results, errorMessage] = useResults();
 
+	const filterResultsByPrice = (price) => {
+		return results.filter((result) => {
+			return result.price === price;
+		});
+	};
 	return (
 		<View style={styles.viewBackground}>
 			<SearchBar
@@ -16,6 +22,9 @@ const SearchScreen = () => {
 			/>
 			{errorMessage ? <Text>{errorMessage}</Text> : null}
 			<Text>{`We have found:${results.length}`}</Text>
+			<ResultsList results={filterResultsByPrice("$")} title="Cost Effective" />
+			<ResultsList results={filterResultsByPrice("$$")} title="Bit Pricier" />
+			<ResultsList results={filterResultsByPrice("$$$")} title="Big Spender" />
 		</View>
 	);
 };
