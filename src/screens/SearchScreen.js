589bf165edb16e9896d8bf4f2c36351d64127dcc
Devesh Avutu <devesh14may@gmail.com>
2020-04-18 16:27:05 +0530

    making initial request in search screen

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Text, View, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
 import yelp from "../apis/yelp";
@@ -6,12 +6,12 @@ const SearchScreen = () => {
 	const [term, setTerm] = useState("");
 	const [results, setResults] = useState([]);
 	const [errorMessage, setErrorMessage] = useState("");
-	const searchApi = async () => {
+	const searchApi = async (searchTerm) => {
 		try {
 			const response = await yelp.get("/search", {
 				params: {
 					limit: 50,
-					term,
+					term: searchTerm,
 					location: "san jose",
 				},
 			});
@@ -20,9 +20,16 @@ const SearchScreen = () => {
 			setErrorMessage("Something went wrong");
 		}
 	};
+	useEffect(() => {
+		searchApi("pasta");
+	}, []);
 	return (
 		<View style={styles.viewBackground}>
-			<SearchBar term={term} onTermChanged={setTerm} onSubmit={searchApi} />
+			<SearchBar
+				term={term}
+				onTermChanged={setTerm}
+				onSubmit={() => searchApi(term)}
+			/>
 			{errorMessage ? <Text>{errorMessage}</Text> : null}
 			<Text>{`We have found:${results.length}`}</Text>
 		</View>
