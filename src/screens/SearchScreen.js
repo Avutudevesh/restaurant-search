b0b83404d343f1f0e4bc9385e305c12c780804cb
Devesh Avutu <devesh14may@gmail.com>
2020-04-18 16:37:11 +0530

    making hooks reusable

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,28 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { Text, View, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
-import yelp from "../apis/yelp";
+import useResults from "../hooks/useResults";
+
 const SearchScreen = () => {
 	const [term, setTerm] = useState("");
-	const [results, setResults] = useState([]);
-	const [errorMessage, setErrorMessage] = useState("");
-	const searchApi = async (searchTerm) => {
-		try {
-			const response = await yelp.get("/search", {
-				params: {
-					limit: 50,
-					term: searchTerm,
-					location: "san jose",
-				},
-			});
-			setResults(response.data.businesses);
-		} catch (err) {
-			setErrorMessage("Something went wrong");
-		}
-	};
-	useEffect(() => {
-		searchApi("pasta");
-	}, []);
+	const [searchApi, results, errorMessage] = useResults();
+
 	return (
 		<View style={styles.viewBackground}>
 			<SearchBar
